9578ef3f9d8b2452f6a6f96e1244f41acb8333b7
brajhanp <arrueta.abrigo@gmail.com>
2025-05-22 11:39:50 -0400

    clase de ejercicios JOIN

---
+++ MOREJoinOperations.sql
@@ -0,0 +1,45 @@
+--Pregunta 1
+SELECT id, title
+ FROM movie
+ WHERE yr=1962;
+
+--Pregunta 2
+select yr from movie where title='Citizen Kane';
+
+--Pregunta 3
+select id, title, yr from movie where title like 'Star Trek%' order by yr;
+
+--Pregunta 4
+select id from actor where name='Glenn Close';
+
+--Pregunta 5
+select id from movie where title='Casablanca';
+
+--Pregunta 6 
+select a.name from casting c inner join actor a on c.actorid =a.id where movieid=11768;
+
+--Pregunta 7
+select a.name from casting c inner join actor a on c.actorid =a.id where movieid=10522;
+
+--Pregunta 8
+select title from casting c left join movie m on c.movieid=m.id
+where actorid=(select id from actor where name="Harrison Ford");
+
+--Pregunta 9
+select title from casting c left join movie m on c.movieid=m.id
+where actorid=(select id from actor where name="Harrison Ford") and c.ord != 1;
+
+--Pregunta 10
+select m.title, a.name from movie m 
+JOIN casting c ON m.id = c.movieid
+JOIN actor a ON c.actorid= a.id
+where c.ord = 1 and m.yr=1962;
+
+--Pregunta 11
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2;
+
